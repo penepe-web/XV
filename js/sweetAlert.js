7c2ee132e13ddbb5a3886d64e855a9262628cbea
Charlyt0 <114592843+Charlyt0@users.noreply.github.com>
2025-08-15 20:25:43 -0300

    Update sweetAlert.js

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,8 +1,8 @@
 Swal.fire({
-title: "¡¡¡Preparad@s para la boda del año!!!",
+title: "¡¡¡Preparad@s para los XV del año!!!",
 confirmButtonText: "Si, estoy preparad@",
 confirmButtonColor: "#ffbba8",
-imageUrl: "img/amigoss.jpg",
+imageUrl: "img/boda.ico",
 imageWidth: "400px",
 imageHeight: "200px",
 widht: "80%",
