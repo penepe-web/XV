eeb58091f6aac18c4567952e7ba053e7d24de7ef
penepe-web <penepe.games@gmail.com>
2025-09-05 14:45:57 -0300

    Update codigo1.js

--- codigo1.js
+++ codigo1.js
@@ -1,15 +1,12 @@
 //ejemplo básico
-$("#btn1").click(function(){
-Swal.fire({
-  title: "0170284440000033103062   DIENTE.MADRE.LUNES",
-  width: 600,
-  padding: "3em",
-  color: "#716add",
-  background: "#fff url(/images/trees.png)",
-  backdrop: `
-    rgba(0,0,123,0.4)
-    url("/images/nyan-cat.gif")
-    left top
-    no-repeat
-  `
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    Swal.fire({
+      title: "0170284440000033103062   DIENTE.MADRE.LUNES",
+      width: 600,
+      padding: "3em",
+      color: "#716add",
+      background: "#fff url(/img/22.png)",
+    });
+  });
 });
