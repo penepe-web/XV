59a4574fbf77b7118490c5dcd01bfa678058be18
penepe-web <penepe.games@gmail.com>
2025-09-05 15:02:38 -0300

    Update codigo1.js

--- codigo1.js
+++ codigo1.js
@@ -2,21 +2,24 @@
 $(document).ready(function(){
   $("#btn1").click(function(){
     Swal.fire({
-        title: `
-    <div style="white-space: nowrap; font-size: 20px; font-weight: bold;">
-      0170284440000033103062
-    </div>
-    <div style="white-space: nowrap; font-size: 18px; margin-top: 10px;">
-      DIENTE.MADRE.LUNES
+  html: `
+    <div style="text-align:center;">
+      <div style="white-space:nowrap; font-size:22px; font-weight:bold; color:#000;">
+        0170284440000033103062
+      </div>
+      <div style="white-space:nowrap; font-size:20px; margin-top:8px; color:#000;">
+        DIENTE.MADRE.LUNES
+      </div>
     </div>
   `,
-      width: 600,
-      padding: "3em",
-      color: "#716add",
-      background: "#cca9dd",
+  width: 600,
+  padding: "2em",
+  background: "#fff url('./img/22.png') no-repeat center center / cover",
+  color: "#716add",
     });
   });
 });
+
 
 
 
