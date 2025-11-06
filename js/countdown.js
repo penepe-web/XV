b387ed2a6db28adc8de2cc68e7fed62e79ff24ed
penepe-web <penepe.games@gmail.com>
2025-11-06 19:31:39 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -6,11 +6,11 @@ month: 11, // required
 
 day: 07, // required
 
-hours: 0, // Default is 0 [0-23] integer
+hours: 20, // Default is 0 [0-23] integer
 
-minutes: 0, // Default is 0 [0-59] integer
+minutes: 59, // Default is 0 [0-59] integer
 
-seconds: 0, // Default is 0 [0-59] integer
+seconds: 59, // Default is 0 [0-59] integer
 
 words: { //words displayed into the countdown
 
